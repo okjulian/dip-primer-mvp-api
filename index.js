38c1d01fa7168722dbed7ce3acceee4e98950bbd
El Juli <mayorga.julian@gmail.com>
2014-03-03 17:54:12 -0300

    feat(index): admitir CORS

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express'),
 app = express(),
-mongo = require('mongoskin');
+mongo = require('mongoskin'),
+cors = require('cors');
 
 var MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/dip-testdb";
+var PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 app.post('/email', function(req, res){
   var db = mongo.db(MONGODB_URL, {safe: true});
@@ -13,5 +16,7 @@ app.post('/email', function(req, res){
     res.json({_id: user[0]._id});
   });
 });
+
+app.listen(PORT);
 
 module.exports = app;
